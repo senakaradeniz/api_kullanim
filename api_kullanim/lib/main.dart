dff0f72ef03d9cefbf9b91dbdaa6987c51ca243c
senakaradeniz <seka.cen@gmail.com>
2023-05-24 09:59:07 +0300

    title changed

--- api_kullanim/lib/main.dart
+++ api_kullanim/lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'API KULLANIMI',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -30,35 +29,29 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
- 
   final url = Uri.parse('https://reqres.in/api/users');
   int? counter;
   var personalResult;
 
-  
-
   @override
-  
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
       body: Center(
         child: ListView.builder(
-          itemCount: counter ,
-          itemBuilder: (context, index) {
-          return ListTile(
-            title: Text( personalResult.data[index].firstName),
-            subtitle: Text(personalResult.data[index].email),
-            leading: CircleAvatar(
-              backgroundImage: 
-              NetworkImage(personalResult.data[index].avatar),
-            ),
-
-          );
-        }),
+            itemCount: counter,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(personalResult.data[index].firstName),
+                subtitle: Text(personalResult.data[index].email),
+                leading: CircleAvatar(
+                  backgroundImage:
+                      NetworkImage(personalResult.data[index].avatar),
+                ),
+              );
+            }),
       ),
     );
   }
